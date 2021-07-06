df4e973683b975099fad2d5dcc57e541a597b7f4
CodeXMakerCompany <codexmaker12@gmail.com>
2021-07-06 13:46:56 -0500

    AAdding buildspec to AWS CI

--- src/components/global/snackbars/success.snackbar.js
+++ src/components/global/snackbars/success.snackbar.js
@@ -4,10 +4,11 @@ import React, { useEffect } from 'react'
 export const SuccessSnackbar = (props) => {
     const { enqueueSnackbar } = useSnackbar();
     const { message, type } = props.content;
-    const options = { variant : type };
     useEffect(() => {
+        const options = { variant : type };
+
         enqueueSnackbar(message, options);
-    }, [message, type, props, enqueueSnackbar, options])
+    }, [message, type, props, enqueueSnackbar])
 
     return (
         <> </>
